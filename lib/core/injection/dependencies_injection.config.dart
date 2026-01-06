5b1b82a1adf826d3ccdcd9db906492ba509574bd
MisaNav <misa@nav-eco.fr>
2026-01-06 16:55:44 +0300

    upgrade dependecies

--- lib/core/injection/dependencies_injection.config.dart
+++ lib/core/injection/dependencies_injection.config.dart
@@ -1,5 +1,5 @@
-// dart format width=80
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format width=80
 
 // **************************************************************************
 // InjectableConfigGenerator
@@ -79,15 +79,24 @@ extension GetItInjectableX on _i174.GetIt {
     gh.lazySingleton<_i460.SharedPreferencesAsync>(
       () => registerModule.prefs,
     );
-    gh.factory<String>(() => registerModule.baseUrl, instanceName: 'BaseUrl');
     gh.singleton<_i420.DbService>(
       () => _i420.DbServiceImp(prefs: gh<_i460.SharedPreferencesAsync>()),
     );
+    gh.factory<String>(() => registerModule.baseUrl, instanceName: 'BaseUrl');
     gh.factory<_i98.PhotonGeometry>(
       () => _i98.PhotonGeometry(
         type: gh<String>(),
         coordinates: gh<List<double>>(),
       ),
+    );
+    gh.factory<_i98.PhotonResponse>(
+      () => _i98.PhotonResponse(
+        type: gh<String>(),
+        features: gh<List<_i98.PhotonFeature>>(),
+      ),
+    );
+    gh.factory<_i98.PhotonException>(
+      () => _i98.PhotonException(gh<String>(), gh<int>()),
     );
     gh.factory<_i98.PhotonProperties>(
       () => _i98.PhotonProperties(
@@ -101,34 +110,13 @@ extension GetItInjectableX on _i174.GetIt {
         housenumber: gh<String>(),
       ),
     );
-    gh.factory<_i98.PhotonFeature>(
-      () => _i98.PhotonFeature(
-        type: gh<String>(),
-        geometry: gh<_i98.PhotonGeometry>(),
-        properties: gh<_i98.PhotonProperties>(),
-      ),
-    );
     gh.lazySingleton<_i361.Dio>(
       () =>
           registerModule.dioNoInterceptor(gh<String>(instanceName: 'BaseUrl')),
       instanceName: 'noInterceptor',
     );
-    gh.factory<_i98.PhotonException>(
-      () => _i98.PhotonException(gh<String>(), gh<int>()),
-    );
     gh.lazySingleton<_i1010.AuthGuard>(
       () => _i1010.AuthGuard(dbService: gh<_i420.DbService>()),
-    );
-    gh.lazySingleton<_i98.PhotonGeocodingService>(
-      () => _i98.PhotonGeocodingService(
-        gh<_i361.Dio>(instanceName: 'noInterceptor'),
-      ),
-    );
-    gh.factory<_i98.PhotonResponse>(
-      () => _i98.PhotonResponse(
-        type: gh<String>(),
-        features: gh<List<_i98.PhotonFeature>>(),
-      ),
     );
     gh.lazySingleton<_i361.Dio>(
       () => registerModule.dio(
@@ -137,54 +125,47 @@ extension GetItInjectableX on _i174.GetIt {
       ),
       instanceName: 'withInterceptor',
     );
-    gh.factory<_i306.RestClient>(
-      () => _i306.RestClient(
-        gh<_i361.Dio>(instanceName: 'withInterceptor'),
-        baseUrl: gh<String>(instanceName: 'BaseUrl'),
-      ),
-    );
-    gh.lazySingleton<_i242.ImageRepository>(
-      () => _i216.ImageRepositoryImpl(rest: gh<_i306.RestClient>()),
-    );
     gh.lazySingleton<_i950.RestaurantWebSocketService>(
       () => _i950.RestaurantWebSocketService(
         dbService: gh<_i420.DbService>(),
         baseUrl: gh<String>(instanceName: 'BaseUrl'),
       ),
     );
-    gh.lazySingleton<_i504.OrdersRepository>(
-      () => _i745.OrdersRepositoryImpl(rest: gh<_i306.RestClient>()),
+    gh.factory<_i98.PhotonFeature>(
+      () => _i98.PhotonFeature(
+        type: gh<String>(),
+        geometry: gh<_i98.PhotonGeometry>(),
+        properties: gh<_i98.PhotonProperties>(),
+      ),
     );
-    gh.lazySingleton<_i97.LanguagesRepository>(
-      () => _i243.LanguagesRepositoryImpl(rest: gh<_i306.RestClient>()),
-    );
-    gh.lazySingleton<_i929.TablesRepository>(
-      () => _i44.TablesRepositoryImpl(rest: gh<_i306.RestClient>()),
-    );
-    gh.lazySingleton<_i518.CategoriesRepository>(
-      () => _i95.CategoriesRepositoryImpl(rest: gh<_i306.RestClient>()),
-    );
-    gh.factory<_i288.LanguagesBloc>(
-      () => _i288.LanguagesBloc(
-        languagesRepository: gh<_i97.LanguagesRepository>(),
+    gh.factory<_i306.RestClient>(
+      () => _i306.RestClient(
+        gh<_i361.Dio>(instanceName: 'withInterceptor'),
+        baseUrl: gh<String>(instanceName: 'BaseUrl'),
       ),
     );
     gh.lazySingleton<_i861.StatsRepository>(
       () => _i430.StatsRepositoryImpl(gh<_i306.RestClient>()),
     );
-    gh.lazySingleton<_i1037.MenusRepository>(
-      () => _i1052.MenusRepositoryImpl(rest: gh<_i306.RestClient>()),
+    gh.lazySingleton<_i442.MenuItemRepository>(
+      () => _i402.MenuItemRepositoryImpl(rest: gh<_i306.RestClient>()),
     );
-    gh.lazySingleton<_i643.AuthRepository>(
-      () => _i345.AuthRepositoryImpl(
-        db: gh<_i420.DbService>(),
-        rest: gh<_i306.RestClient>(),
+    gh.lazySingleton<_i504.OrdersRepository>(
+      () => _i745.OrdersRepositoryImpl(rest: gh<_i306.RestClient>()),
+    );
+    gh.factory<_i406.StatsBloc>(
+      () => _i406.StatsBloc(statsRepository: gh<_i861.StatsRepository>()),
+    );
+    gh.lazySingleton<_i242.ImageRepository>(
+      () => _i216.ImageRepositoryImpl(rest: gh<_i306.RestClient>()),
+    );
+    gh.lazySingleton<_i98.PhotonGeocodingService>(
+      () => _i98.PhotonGeocodingService(
+        gh<_i361.Dio>(instanceName: 'noInterceptor'),
       ),
     );
-    gh.factory<_i562.CategoriesBloc>(
-      () => _i562.CategoriesBloc(
-        categoriesRepository: gh<_i518.CategoriesRepository>(),
-      ),
+    gh.lazySingleton<_i518.CategoriesRepository>(
+      () => _i95.CategoriesRepositoryImpl(rest: gh<_i306.RestClient>()),
     );
     gh.lazySingleton<_i986.RestaurantRepository>(
       () => _i659.RestaurantRepositoryImpl(
@@ -192,34 +173,53 @@ extension GetItInjectableX on _i174.GetIt {
         db: gh<_i420.DbService>(),
       ),
     );
-    gh.factory<_i419.TableBloc>(
-      () => _i419.TableBloc(tablesRepository: gh<_i929.TablesRepository>()),
+    gh.lazySingleton<_i929.TablesRepository>(
+      () => _i44.TablesRepositoryImpl(rest: gh<_i306.RestClient>()),
     );
-    gh.factory<_i788.AuthBloc>(
-      () => _i788.AuthBloc(gh<_i643.AuthRepository>()),
-    );
-    gh.lazySingleton<_i442.MenuItemRepository>(
-      () => _i402.MenuItemRepositoryImpl(rest: gh<_i306.RestClient>()),
-    );
-    gh.factory<_i789.MenusBloc>(
-      () => _i789.MenusBloc(menusRepository: gh<_i1037.MenusRepository>()),
-    );
-    gh.factory<_i414.OrdersBloc>(
-      () => _i414.OrdersBloc(repo: gh<_i504.OrdersRepository>()),
-    );
-    gh.factory<_i528.OrderMenuItemBloc>(
-      () => _i528.OrderMenuItemBloc(repo: gh<_i504.OrdersRepository>()),
-    );
-    gh.factory<_i406.StatsBloc>(
-      () => _i406.StatsBloc(statsRepository: gh<_i861.StatsRepository>()),
-    );
-    gh.factory<_i864.RestaurantBloc>(
-      () => _i864.RestaurantBloc(restaurant: gh<_i986.RestaurantRepository>()),
+    gh.lazySingleton<_i97.LanguagesRepository>(
+      () => _i243.LanguagesRepositoryImpl(rest: gh<_i306.RestClient>()),
     );
     gh.factory<_i910.MenuItemBloc>(
       () => _i910.MenuItemBloc(
         repo: gh<_i442.MenuItemRepository>(),
         imageRepo: gh<_i242.ImageRepository>(),
+      ),
+    );
+    gh.lazySingleton<_i1037.MenusRepository>(
+      () => _i1052.MenusRepositoryImpl(rest: gh<_i306.RestClient>()),
+    );
+    gh.factory<_i288.LanguagesBloc>(
+      () => _i288.LanguagesBloc(
+        languagesRepository: gh<_i97.LanguagesRepository>(),
+      ),
+    );
+    gh.factory<_i864.RestaurantBloc>(
+      () => _i864.RestaurantBloc(restaurant: gh<_i986.RestaurantRepository>()),
+    );
+    gh.lazySingleton<_i643.AuthRepository>(
+      () => _i345.AuthRepositoryImpl(
+        db: gh<_i420.DbService>(),
+        rest: gh<_i306.RestClient>(),
+      ),
+    );
+    gh.factory<_i789.MenusBloc>(
+      () => _i789.MenusBloc(menusRepository: gh<_i1037.MenusRepository>()),
+    );
+    gh.factory<_i788.AuthBloc>(
+      () => _i788.AuthBloc(gh<_i643.AuthRepository>()),
+    );
+    gh.factory<_i419.TableBloc>(
+      () => _i419.TableBloc(tablesRepository: gh<_i929.TablesRepository>()),
+    );
+    gh.factory<_i528.OrderMenuItemBloc>(
+      () => _i528.OrderMenuItemBloc(repo: gh<_i504.OrdersRepository>()),
+    );
+    gh.factory<_i414.OrdersBloc>(
+      () => _i414.OrdersBloc(repo: gh<_i504.OrdersRepository>()),
+    );
+    gh.factory<_i562.CategoriesBloc>(
+      () => _i562.CategoriesBloc(
+        categoriesRepository: gh<_i518.CategoriesRepository>(),
       ),
     );
     return this;
