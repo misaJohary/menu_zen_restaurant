0e5b0f49012894d54dea8b34060da8f60fc898b4
MisaNav <misa@nav-eco.fr>
2026-04-17 12:00:17 +0300

    add kitchens feature

--- apps/menu_zen_mobile/test/widget_test.dart
+++ apps/menu_zen_mobile/test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:menu_zen_mobile/app.dart';
 
-import 'package:menu_zen_mobile/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
